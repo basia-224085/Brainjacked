65d2cc3a3ed185b7120e673a70e607f8984051f4
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2021-04-17 21:31:40 +0200

    brain menu

--- Brainjacked/app/src/main/java/com/hackaton/brainjacked/BrainActivity.java
+++ Brainjacked/app/src/main/java/com/hackaton/brainjacked/BrainActivity.java
@@ -3,6 +3,8 @@ package com.hackaton.brainjacked;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class BrainActivity extends AppCompatActivity {
@@ -11,5 +13,13 @@ public class BrainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.brain_activity);
+
+        Button memory = findViewById(R.id.memory);
+        Button languages = findViewById(R.id.languages);
+        Button emotions = findViewById(R.id.emotions);
+        Button logic = findViewById(R.id.logic);
+        Button my_brain = findViewById(R.id.my_brain);
+        Button sleep = findViewById(R.id.sleep);
+
     }
 }
