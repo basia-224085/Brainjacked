0f80cc507f8977b4d1317dba667867b6b73813c8
Matix97 <mateusz_baranski@hotmail.com>
2021-04-17 22:14:55 +0200

    adding image

--- Brainjacked/app/src/main/java/com/hackaton/brainjacked/services/ServiceGenerator.java
+++ Brainjacked/app/src/main/java/com/hackaton/brainjacked/services/ServiceGenerator.java
@@ -8,7 +8,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
-    public static final String API_BASE_URL = "http://naszback:8080/";
+    public static final String API_BASE_URL = "https://brainjacked.herokuapp.com/humans/";
 
     public static String bearerToken;
     public static String role;
