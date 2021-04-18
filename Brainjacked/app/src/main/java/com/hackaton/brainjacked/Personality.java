0eb8a09e718145441979232d5329746fa2bc7d4a
kapj-10 <kapj-10@sr.dmcs.pl>
2021-04-18 05:30:30 +0200

    Form activity

---
+++ Brainjacked/app/src/main/java/com/hackaton/brainjacked/Personality.java
@@ -0,0 +1,12 @@
+package com.hackaton.brainjacked;
+
+public class Personality {
+    static class Letters {
+        public char firstLetter;
+        public char secondLetter;
+        public char thirdLetter;
+        public char fourthLetter;
+    }
+
+
+}
