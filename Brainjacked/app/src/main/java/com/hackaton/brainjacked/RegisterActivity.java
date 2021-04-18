d00adfe92cc7c22a21168190d22c1b916e873266
Matix97 <mateusz_baranski@hotmail.com>
2021-04-18 09:48:14 +0200

    adding validation if user is human

--- Brainjacked/app/src/main/java/com/hackaton/brainjacked/RegisterActivity.java
+++ Brainjacked/app/src/main/java/com/hackaton/brainjacked/RegisterActivity.java
@@ -1,13 +1,23 @@
 package com.hackaton.brainjacked;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hackaton.brainjacked.DTO.Login;
+import com.hackaton.brainjacked.DTO.LoginReturn;
+import com.hackaton.brainjacked.services.ServiceGenerator;
+import com.hackaton.brainjacked.services.UserService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,25 +31,55 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button registerButton = findViewById(R.id.register_button);
         registerButton.setOnClickListener(v -> {
-            if(!password.getText().toString().equals(confirmPassword.getText().toString())) {
+            String pass = password.getText().toString();
+            String chip = implantNr.getText().toString();
+            if (!pass.equals(confirmPassword.getText().toString())) {
                 toast("Passwords do not match");
-            } else if(!implantNr.getText().toString().matches("[0-9]+")) {
+            } else if (!chip.matches("[0-9]+")) {
                 toast("Implant number must contain only numbers");
             }
-            // TODO: add new user to the database
-            Intent intent = new Intent(this, FormActivity.class);
-            intent.putExtra("chip",implantNr.getText().toString());
-            intent.putExtra("pass", confirmPassword.getText().toString());
-            startActivity(intent);
+            registerIfUserIsHuman(chip, pass);
         });
         Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(v -> {
-            startActivity(new Intent(this, LoginActivity.class));
-        });
+        loginButton.setOnClickListener(v -> startActivity(new Intent(this, LoginActivity.class)));
     }
 
     void toast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
+    }
+
+    void registerIfUserIsHuman(String implantNr, String password) {
+        Login login = new Login(implantNr, password);
+        UserService userService = ServiceGenerator.createService(UserService.class);
+        Call<LoginReturn> call = userService.login(login);
+        call.enqueue(new Callback<LoginReturn>() {
+            @Override
+            public void onResponse(Call<LoginReturn> call, Response<LoginReturn> response) {
+                Log.e("MY", String.valueOf(response.code()));
+                if (response.code() == 201) {
+                    Intent intent = new Intent(getBaseContext(), FormActivity.class);
+                    intent.putExtra("chip", implantNr);
+                    intent.putExtra("pass", password);
+                    startActivity(intent);
+
+                } else
+                    new AlertDialog.Builder(RegisterActivity.this)
+                            .setTitle("ERROR")
+                            .setMessage("You aren't HUMAN")
+                            .setNegativeButton(android.R.string.ok, null)
+                            .show();
+
+            }
+
+            @Override
+            public void onFailure(Call<LoginReturn> call, Throwable t) {
+                new AlertDialog.Builder(RegisterActivity.this)
+                        .setTitle("ERROR")
+                        .setMessage("You aren't HUMAN")
+                        .setNegativeButton(android.R.string.ok, null)
+                        .show();
+            }
+        });
     }
 }
